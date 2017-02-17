f3b5976d8e44a19805f6e7d2d17e5d3d89f47907
Rose Kariuki <rosekariuki28@gmail.com>
2017-02-17 03:51:09 +0000

    highest number cubed

--- highest_number_cubed/main.py
+++ highest_number_cubed/main.py
@@ -2,4 +2,12 @@
 
 
 def highest_number_cubed(limit):
-    pass
+    #the highest number cubed that is still less than the input 'limit'
+    number = 1
+    while number ** 3 < limit:
+        number += 1
+        if number ** 3 > limit:
+            return number-1
+    
+
+    
